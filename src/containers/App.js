a7160e334364833df94b51458ccf0a6f73373eaa
daniel.luan.tran@gmail.com <daniel.luan.tran@gmail.com>
2021-01-15 10:58:48 +0700

    Update Scroll Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import './App.css'
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import './App.css';
+import Scroll from "../components/Scroll";
 
 class App extends Component {
     constructor () {
@@ -23,11 +24,12 @@ class App extends Component {
     }
 
     render () {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {robots, searchfield} = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
         console.log(filteredRobots);
-        if (this.state.robots.length === 0) {
+        if (robots.length === 0) {
             return <h1>LOADING</h1>
         }
         else {
@@ -35,7 +37,9 @@ class App extends Component {
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange = {this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
